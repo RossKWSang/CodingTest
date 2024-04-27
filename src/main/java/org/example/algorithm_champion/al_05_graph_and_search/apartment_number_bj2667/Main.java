b2c0fcf7c6f17ed8d975629e5cbb4397f8e64dac
Wonsang Kim <rosskysoss@gmail.com>
2024-04-27 21:06:39 +0900

    graph searching

---
+++ src/main/java/org/example/algorithm_champion/al_05_graph_and_search/apartment_number_bj2667/Main.java
@@ -0,0 +1,106 @@
+package org.example.algorithm_champion.al_05_graph_and_search.apartment_number_bj2667;
+
+/* 격자항 그래프 구성이 핵심
+*
+*
+* */
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static FastReader scan = new FastReader();
+    static StringBuilder sb = new StringBuilder();
+
+    static int N, group_cnt;
+    static String[] a;
+    static boolean[][] visit;
+    static int[][] dir = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
+    static ArrayList<Integer> group;
+
+    static void input() {
+        N = scan.nextInt();
+        a = new String[N];
+        for (int i = 0; i < N; i++) {
+            a[i] = scan.nextLine();
+        }
+        visit = new boolean[N][N];
+    }
+
+    static void dfs(int x, int y) {
+        group_cnt++;
+        visit[x][y] = true;
+        for (int k = 0; k < 4; k++) {
+            int nx = x + dir[k][0];
+            int ny = y + dir[k][1];
+            if (nx < 0 || ny < 0 || nx >= N || ny >= N) continue;
+            if (a[nx].charAt(ny) == '0') continue;
+            if (visit[nx])
+        }
+    }
+
+    static void pro() {
+        group = new ArrayList<>();
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if (!visit[i][j] && a[i].charAt(j) == '1') {
+
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        input();
+        pro();
+    }
+
+
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        public FastReader(String s) throws FileNotFoundException {
+            br = new BufferedReader(new FileReader(new File(s)));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+}
