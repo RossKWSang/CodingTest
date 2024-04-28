0d3e175e5dcfa2603e2ef7687582c63b687dbafd
Wonsang Kim <rosskysoss@gmail.com>
2024-04-28 17:30:15 +0900

    graph solution

--- src/main/java/org/example/algorithm_champion/al_05_graph_and_search/apartment_number_bj2667/Main.java
+++ src/main/java/org/example/algorithm_champion/al_05_graph_and_search/apartment_number_bj2667/Main.java
@@ -7,6 +7,7 @@ package org.example.algorithm_champion.al_05_graph_and_search.apartment_number_b
 
 import java.io.*;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -37,7 +38,8 @@ public class Main {
             int ny = y + dir[k][1];
             if (nx < 0 || ny < 0 || nx >= N || ny >= N) continue;
             if (a[nx].charAt(ny) == '0') continue;
-            if (visit[nx])
+            if (visit[nx][ny]) continue;
+            dfs(nx, ny);
         }
     }
 
@@ -46,17 +48,23 @@ public class Main {
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < N; j++) {
                 if (!visit[i][j] && a[i].charAt(j) == '1') {
-
+                    group_cnt = 0;
+                    dfs(i, j);
+                    group.add(group_cnt);
                 }
             }
         }
+
+        Collections.sort(group);
+        sb.append(group.size()).append('\n');
+        for (int cnt: group) sb.append(cnt).append('\n');
+        System.out.println(sb.toString());
     }
 
     public static void main(String[] args) {
         input();
         pro();
     }
-
 
     static class FastReader {
         BufferedReader br;
